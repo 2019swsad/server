63e13f2802c2dee0201561509a72986e5717300e
zml <2535044931@qq.com>
2019-05-28 23:38:40 +0800

    upload task v1

---
+++ app/controllers/taskController.js
@@ -0,0 +1,48 @@
+const url='localhost:27017/data'
+const Joi = require('joi'),
+    monk=require('monk'),
+    ObjectId=require('mongodb').ObjectID,
+    uuid=require('uuid/v4'),
+    Router = require('koa-router'),
+    passport=require('koa-passport')
+
+// Task schema
+const taskRegSchema = Joi.object().keys({
+        title:Joi.string().alphanum().min(4).max(60).trim().required(),
+        salary:Joi.number().integer().min(1),
+        describtion:Joi.string(),
+        beginTime:Joi.date().min('now'),
+        expireTime:Joi.date().min(Joi.ref('beginTime')).required(),
+        participantNum:Joi.number().integer().min(1);
+    });
+
+const db=monk(url)
+db.then(()=>{console.log('Linked to DB');})
+const collection = db.get('Task')
+
+const walletRouter=new Router({prefix:'/task'})
+walletRouter
+    .get('/create',         check,  createTask)
+
+function check(ctx, next) {
+    if (ctx.isAuthenticated()) {
+      return next()
+    } else {
+      ctx.redirect('/')
+    }
+}
+
+/**
+ * @example curl -XPOST "http://localhost:8081/task/create" -d '{"title":"New task"}' -H 'Content-Type: application/json'
+ */
+async function createWallet (ctx, next) {
+    let passData = await Joi.validate(ctx.request.body, taskRegSchema)
+    //passData.uid=uuid()
+    console.log(passData)
+    ctx.body=await collection.insert(passData).then((doc)=>{return true})
+    ctx.status = 201;
+    //ctx.redirect('/')
+    await next();
+}
+
+module.exports=walletRouter
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router'),
     combineRouters =require('koa-combine-routers'),
     userRouter = require('../controllers/indexController'),
     walletRouter=require('../controllers/walletController'),
+    taskRouter=require('../controllers/taskController'),
     {
         renderIndex,
         renderTest
@@ -11,7 +12,7 @@ const indexRouter = new Router();
 indexRouter
     .get('/',renderIndex)
     .get('/test',renderTest)
-        
+
 const router=combineRouters (
     userRouter,
     indexRouter,
