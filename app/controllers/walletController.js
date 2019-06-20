fa1a9c167d166f538beac575fd62e2af335a5e74
ZZFM <flyingman_pan@outlook.com>
2019-06-20 19:36:33 +0800

    fix

--- app/controllers/walletController.js
+++ app/controllers/walletController.js
@@ -51,9 +51,13 @@ async function createWalletWeb (ctx, next) {
  * @example curl -XGET "http://localhost:8081/wallet/deposit/:amount"
  */
 async function depositWallet (ctx, next) {
-    console.log(ctx.params.num);
-    balance=walletDB.find({uid:ctx.state.user[0].uid}).then((doc)=>{return doc.balance})
+    console.log('params'+ctx.params.num);
+    balance=await walletDB.find({uid:ctx.state.user[0].uid}).then((doc)=>{return doc})
+    console.log('balance'+balance)
+    
     fin=Number(balance)+Number(ctx.params.num)
+    console.log('fin'+fin)
+    
     ctx.body=await walletDB
         .findOneAndUpdate({uid:ctx.state.user[0].uid},{$set:{balance:fin}})
         .then((doc)=>{if(doc.length!==0) return true; else return false})
