2a39bee2f0a66b032dbf82242a14bf33c8d79b5e
ZZThinkPad <flyingman_pan@outlook.com>
2019-06-26 12:46:10 +0800

    add compress

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -2,7 +2,8 @@ const fs = require('fs'),
     path = require('path'),
     send = require('koa-send'),
     Router = require('koa-router'),
-    { check } = require('../helpers/auth')
+    { check } = require('../helpers/auth'),
+    compress_img = require('compress-images')
 
 const fileRouter = new Router({ prefix: '/file' })
 fileRouter
@@ -11,9 +12,26 @@ fileRouter
 
 async function handleUpload(ctx, next) {
     const file = ctx.request.files.file
+    console.log(file)
+    
     const reader = fs.createReadStream(file.path)
     const stream = fs.createWriteStream(path.join('./upload/', ctx.state.user[0].uid + '.jpg'))
     reader.pipe(stream)
+
+    //compress
+    pathToImg = './upload/' + ctx.state.user[0].uid + '.jpg'
+    compress_img(pathToImg, './imgtest/', { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+        { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
+        { svg: { engine: 'svgo', command: '--multipass' } },
+        { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
+        (err, completed, statistic) => {
+            console.log('-------------');
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log('-------------');
+        })
     ctx.status = 200
     await next()
 }
