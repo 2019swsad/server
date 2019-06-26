047971cfc2dfc572864667e9769aebc95ea6ae0d
ZZThinkPad <flyingman_pan@outlook.com>
2019-06-26 14:22:09 +0800

    no fix

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -4,7 +4,7 @@ const fs = require('fs'),
     Router = require('koa-router'),
     { check } = require('../helpers/auth'),
     compress_img = require('compress-images'),
-    {convert}=require('easyimage')
+    { convert } = require('easyimage')
 
 const fileRouter = new Router({ prefix: '/file' })
 fileRouter
@@ -14,33 +14,33 @@ fileRouter
 async function handleUpload(ctx, next) {
     const file = ctx.request.files.file
     console.log(file);
-    let aftername=file.type.replace(/\w+\//, '.');
+    let aftername = file.type.replace(/\w+\//, '.');
     const reader = fs.createReadStream(file.path)
-    const stream = fs.createWriteStream(path.join('./upload/', ctx.state.user[0].uid + aftername))
+    const stream = fs.createWriteStream(path.join('./upload/', ctx.state.user[0].uid + '.png'))
     reader.pipe(stream)
     //compress
-    
-    
+    // pathToImg = './upload/' + ctx.state.user[0].uid + aftername
+    // compress_img(pathToImg, './imgtest/', { compress_force: true, statistic: true, autoupdate: true }, false,
+    //     { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+    //     { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
+    //     { svg: { engine: 'svgo', command: '--multipass' } },
+    //     { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
+    //     (err, completed, statistic) => {
+    //         console.log('-------------');
+    //         console.log(err);
+    //         console.log(completed);
+    //         console.log(statistic);
+    //         console.log('-------------');
+    //     })
+
     ctx.status = 200
-    
+
     await next()
-    pathToImg = './upload/' + ctx.state.user[0].uid + aftername
-    compress_img(pathToImg, './imgtest/', { compress_force: true, statistic: true, autoupdate: true }, false,
-        { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
-        { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
-        { svg: { engine: 'svgo', command: '--multipass' } },
-        { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
-        (err, completed, statistic) => {
-            console.log('-------------');
-            console.log(err);
-            console.log(completed);
-            console.log(statistic);
-            console.log('-------------');
-        })
+
 }
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
     })
 }
 async function handleFetch(ctx, next) {
