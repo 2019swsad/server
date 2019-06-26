613784690a53c98c8edcf790833ce3aa018f7058
ZZThinkPad <flyingman_pan@outlook.com>
2019-06-26 13:51:43 +0800

    fix

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -13,28 +13,32 @@ fileRouter
 async function handleUpload(ctx, next) {
     const file = ctx.request.files.file
     console.log(file);
-    let aftername = file.type.replace(/\w+\//, '.');
+    let aftername=file.type.replace(/\w+\//, '.');
     const reader = fs.createReadStream(file.path)
     const stream = fs.createWriteStream(path.join('./upload/', ctx.state.user[0].uid + aftername))
-    reader.pipe(stream).then(() => {
-        pathToImg = './upload/' + ctx.state.user[0].uid + aftername
-        compress_img(pathToImg, './imgtest/', { compress_force: false, statistic: true, autoupdate: true }, false,
-            { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
-            { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
-            { svg: { engine: 'svgo', command: '--multipass' } },
-            { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
-            (err, completed, statistic) => {
-                console.log('-------------');
-                console.log(err);
-                console.log(completed);
-                console.log(statistic);
-                console.log('-------------');
-            })
-    })
+    reader.pipe(stream)
     //compress
-
+    sleep(50)
+    pathToImg = './upload/' + ctx.state.user[0].uid + aftername
+    compress_img(pathToImg, './imgtest/', { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+        { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
+        { svg: { engine: 'svgo', command: '--multipass' } },
+        { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
+        (err, completed, statistic) => {
+            console.log('-------------');
+            console.log(err);
+            console.log(completed);
+            console.log(statistic);
+            console.log('-------------');
+        })
     ctx.status = 200
     await next()
+}
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
 }
 async function handleFetch(ctx, next) {
     ctx.cacheControl('max-age=3600')
