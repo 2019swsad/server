d40aa1c069f656b32b3649831b396306a862e829
ZZThinkPad <flyingman_pan@outlook.com>
2019-06-26 16:57:02 +0800

    fix finish task return func

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -5,9 +5,9 @@ const Joi = require('joi'),
   db = require('../helpers/db'),
   { check, isSelfOp } = require('../helpers/auth'),
   { getNow, isEarly } = require('../helpers/date'),
-  {countOrder, payByTask, noticeNotFinish, setOrderFinish} = require('../helpers/orderHelper'),
+  { countOrder, payByTask, noticeNotFinish, setOrderFinish } = require('../helpers/orderHelper'),
   { updateUserFunc } = require('./userController'),
-  { createWallet, transferFunc } = require('../helpers/walletHelper'),
+  { createWallet, transferFunc, queryBalance } = require('../helpers/walletHelper'),
   { queryPerson } = require('../helpers/userHelper'),
   { queryMsgList, createMsg } = require('../helpers/msgHelper')
 
@@ -60,7 +60,7 @@ taskRouter
 
 
 async function getParticipator(ctx, next) {
-  res = await orderDB.find({ tid: ctx.params.id,status:{$in:['进行中','已完成','已评价'] }}).then((docs) => { return docs })
+  res = await orderDB.find({ tid: ctx.params.id, status: { $in: ['进行中', '已完成', '已评价'] } }).then((docs) => { return docs })
   //ctx.body = await userDB.find({uid:res.uid}).then((docs)=>{return docs})
   ctx.body = res
   console.log(ctx.body)
@@ -75,10 +75,13 @@ async function setTaskFinish(ctx, next) {
   res = await taskDB.findOneAndUpdate({ tid: ctx.params.id }, { $set: { status: "已结束" } }).then((doc) => { return doc })
   res.status = "已结束"
   ctx.body = res.status
-  let orders = await orderDB.find({tid:res.tid}).then((doc)=>{return doc})
-  for(let j of orders){
-    await orderDB.findOneAndUpdate({oid:j.oid,status:{$in:['进行中','候补中'] }},{$set:{status:"已失效"}}).then((doc)=>{return doc})
+  let orders = await orderDB.find({ tid: res.tid }).then((doc) => { return doc })
+  for (let j of orders) {
+    await orderDB.findOneAndUpdate({ oid: j.oid, status: { $in: ['进行中', '候补中'] } }, { $set: { status: "已失效" } }).then((doc) => { return doc })
   }
+  let amount = await queryBalance(res.tid)
+  transferFunc(res.tid,res.uid,amount)
+
   ctx.status = 201
   console.log(res)
   await next()
@@ -91,9 +94,9 @@ async function setOnGoing(ctx, next) {
   res = await taskDB.findOneAndUpdate({ tid: ctx.params.id }, { $set: { status: "进行中" } }).then((doc) => { return doc })
   res.status = "进行中"
   ctx.body = res.status
-  let orders = await orderDB.find({tid:res.tid}).then((doc)=>{return doc})
-  for(let j of orders){
-    await orderDB.findOneAndUpdate({oid:j.oid,status:"候补中"},{$set:{status:"已失效"}}).then((doc)=>{return doc})
+  let orders = await orderDB.find({ tid: res.tid }).then((doc) => { return doc })
+  for (let j of orders) {
+    await orderDB.findOneAndUpdate({ oid: j.oid, status: "候补中" }, { $set: { status: "已失效" } }).then((doc) => { return doc })
   }
   ctx.status = 201
   console.log(res)
